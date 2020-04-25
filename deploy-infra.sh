bd86027d1b58c085a5b54b8c334ca5fa3abfdc10
Rahul Verma <rahulv@127.0.0.1localhostindia-labs.com>
2020-04-25 21:17:10 +0530

    update code deployment automation

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -8,6 +8,12 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
 --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
 --region $REGION \
@@ -17,7 +23,13 @@ aws cloudformation deploy \
 --no-fail-on-empty-changeset \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides \
-CodePipelineBucket=$CODEPIPELINE_BUCKET
+  EC2InstanceType=$EC2_INSTANCE_TYPE \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
