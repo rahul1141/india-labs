bdaff42147284adadef5cb53da4fc6e9516ba65f
Rahul Verma <rahulv@127.0.0.1localhostindia-labs.com>
2020-04-25 17:38:55 +0530

    Add code deployment automation

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,6 +3,21 @@ STACK_NAME=indialabs-stack
 REGION=us-east-2
 CLI_PROFILE=india-labs-profile
 EC2_INSTANCE_TYPE=t2.micro
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+--query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+--region $REGION \
+--profile $CLI_PROFILE \
+--stack-name $STACK_NAME-setup \
+--template-file setup.yml \
+--no-fail-on-empty-changeset \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides \
+CodePipelineBucket=$CODEPIPELINE_BUCKET
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
@@ -18,5 +33,5 @@ EC2InstanceType=$EC2_INSTANCE_TYPE
 if [ $? -eq 0 ]; then
 aws cloudformation list-exports \
 --profile $CLI_PROFILE \
---query "Exports[?Name=='InstanceEndpoint'].Value"
+--query "Exports[?Name=='InstanceDNS'].Value"
 fi
