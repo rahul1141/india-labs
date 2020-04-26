ab47bca42478b5e0fc2d9038a6bdfb8bdae751ff
Rahul Verma <rahulv@127.0.0.1localhostindia-labs.com>
2020-04-26 11:29:53 +0530

    update deployment script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,17 +4,18 @@ REGION=us-east-2
 CLI_PROFILE=india-labs-profile
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile india-labs-profile \
 --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 # Generate a personal access token with repo and admin:repo_hook
 # permissions from https://github.com/settings/tokens
-GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
-GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
-GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_ACCESS_TOKEN=$(cat ~/.github/india-labs-access-token)
+GH_OWNER=$(cat ~/.github/india-labs-owner)
+GH_REPO=$(cat ~/.github/india-labs-repo)
 GH_BRANCH=master
 echo -e "\n\n=========== Deploying setup.yml ==========="
+
 aws cloudformation deploy \
 --region $REGION \
 --profile $CLI_PROFILE \
@@ -40,7 +41,12 @@ aws cloudformation deploy \
 --no-fail-on-empty-changeset \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides \
-EC2InstanceType=$EC2_INSTANCE_TYPE
+  EC2InstanceType=$EC2_INSTANCE_TYPE \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 if [ $? -eq 0 ]; then
 aws cloudformation list-exports \
