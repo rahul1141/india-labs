1912db9e3898e6a2feff8b4086fbfb2c1bcf6f5a
Rahul Verma <rahulv@127.0.0.1localhostindia-labs.com>
2020-04-26 21:36:46 +0530

    add coming soon

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const { hostname } = require('os');
-const http = require('http');
-const message = 'Hello Cloud deployemnt\n';
-const port = 8080;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  console.log('req received' + req);
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(message);
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname()}:${port}/`);
-});
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, () => console.log('Server running on 8080...'));
+
+// const http = require('http');
+// const message = 'Hello Cloud deployemnt\n';
+// const port = 8080;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   console.log('req received' + req);
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end(message);
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname()}:${port}/`);
+// });
